5ed4df7ffaa194019ec10ac030494a3e107f561e
Kelsey Brennan <brennank@mail.gvsu.edu>
2016-10-16 14:10:11 -0400

    Better obstacle movement

--- src/edu/gvsu/scis/cis350/Obstacle.java
+++ src/edu/gvsu/scis/cis350/Obstacle.java
@@ -1,19 +1,16 @@
 package edu.gvsu.scis.cis350;
 
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import java.io.File;
-
-import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 
 /**
  * Methods to control an obstacle in the game.
  * @author Kelsey
  *
  */
-public class Obstacle {
+public class Obstacle extends JLabel {
+	private static final long serialVersionUID = 0;
 
 	/** x-coordinate for left edge of obstacle. */
 	private int leftX;
@@ -24,37 +21,33 @@ public class Obstacle {
 	/** y-coordinate for height of obstacle. */
 	private int y;
 	
-	/** Speed obstacle will move at. */
-	private int speed;
-	
 	/** Image for obstacle. */
-	private BufferedImage image;
+	private ImageIcon image;
 
 
 	/**
-	 * Create a new obstacle width the specificed width and height.
-	 * @param bgDimension The dimensions of the background image
-	 * @param pSpeed The speed the obstacle will move at
+	 * Create a new obstacle width the specified width and height.
+	 * @param bg The dimensions of the background image
 	 */
-	public Obstacle(final Dimension bgDimension, final int pSpeed) {
-		// Load the image for the obstacle.
+	public Obstacle(final Dimension bg) {
+
 		try {
-			image = ImageIO.read(new File(
-					"Graphics/Obstacles/obstacle.png"));
+			image = new ImageIcon("Graphics/Obstacles/obstacle.png");
 		} catch (Exception e) {
 			System.out.println("Failed to load obstacle image");
 			return;
 		}
-				
-		// Assign the dimensions of the obstacle based on image.
-		leftX = bgDimension.width;
-		rightX = leftX + image.getWidth();
-		y = bgDimension.height - image.getHeight();
-		//y = image.getHeight();
-		speed = pSpeed;
-			
+
+		this.setIcon(image);
+		
+		leftX = bg.width;
+		rightX = bg.width + image.getIconWidth();
+		y = bg.height - image.getIconHeight();
+		
+		this.setLocation(leftX, y);	
 	}
 
+	
 	/**
 	 * Move the obstacle towards the left of the screen by the increment.
 	 * @param increment the desired amount to move by
@@ -64,6 +57,9 @@ public class Obstacle {
 			leftX = leftX - increment;
 			rightX = rightX - increment;
 		}	
+		
+		this.setLocation(leftX, y);
+		System.out.println(this.getLocation());
 	}
 
 	
@@ -75,23 +71,6 @@ public class Obstacle {
 		return (rightX >= 0);
 	}
 	
-	
-	/**
-	 * Draws the obstacle on the screen and moves its position.
-	 * @param window The window to draw the obstacle on.
-	 */
-	public final void draw(final Graphics window) {
-		// Clear the previous drawing of the obstacle
-		window.clearRect(this.getLeftX() + 1, this.getY(), 
-				image.getWidth(), image.getHeight());
-		
-		// Draw the new obstacle
-		window.drawImage(image, this.getLeftX(), this.getY(), image.getWidth(), 
-        		image.getHeight(), null);
-				
-		// Move the obstacle into the next position
-		this.move(speed);
-	}
 	
 	/**
 	 * Returns left x coordinate of obstacle.
@@ -116,21 +95,5 @@ public class Obstacle {
 	 */
 	public final int getY() {
 		return this.y;
-	}
-	
-	/**
-	 * Returns width of obstacle image.
-	 * @return integer width
-	 */
-	public final int getWidth() {
-		return image.getWidth();
-	}
-	
-	/**
-	 * Returns height of obstacle image.
-	 * @return integer height
-	 */
-	public final int getHeight() {
-		return image.getHeight();
 	}
 }
